2574b8109e5d32fe1fbd8be0da6f6944bf633e2a
Junghoon Noh <jungnoh.dev@gmail.com>
2026-01-26 16:11:26 +0000

    pylib: Add test target tools

---
+++ pylib/target_actions.py
@@ -0,0 +1,81 @@
+from target_run import SshTarget, PgTarget, ssh_command, pg_file, pg_queries, LogConfig
+from typing import Dict, Optional
+import subprocess
+from pathlib import Path
+
+CMD_DISABLE_SWAP = "sudo swapoff -a"
+CMD_SYNC = "sudo sync"
+CMD_DROP_CACHES = "sudo sh -c 'echo 1 > /proc/sys/vm/drop_caches'"
+
+
+def cmd_pg_ctlcuster_restart(version: str, name: str = "main") -> str:
+    """
+    Returns the command to restart the PostgreSQL cluster.
+    """
+    return f"sudo pg_ctlcluster {version} {name} restart"
+
+
+def ssh_get_memory_size(target: SshTarget) -> int:
+    """
+    Returns the memory size in GB.
+    """
+    result = ssh_command(target, "cat /proc/meminfo | grep MemTotal | awk '{print $2}'")
+    return int(result.stdout.strip())
+
+
+def pg_clear_stats(target: PgTarget, log: Optional[LogConfig] = None) -> subprocess.CompletedProcess:
+    """
+    Clears the statistics for the PostgreSQL database.
+    """
+    return pg_queries(target, [
+        "SELECT pg_stat_statements_reset();",
+        "SELECT pg_stat_kcache_reset();",
+        "SELECT pg_wait_sampling_reset_profile();",
+        "SELECT pg_stat_reset();",
+    ], log)
+
+
+def pg_print_stats(target: PgTarget, log: Optional[LogConfig] = None) -> subprocess.CompletedProcess:
+    """
+    Prints the statistics for the PostgreSQL database.
+    """
+    return pg_queries(target, [
+        "SELECT * FROM pg_stat_bgwriter;",
+        "SELECT * FROM pg_stat_io;",
+        "SELECT * FROM pg_stat_wal;",
+    ], log)
+
+
+def pg_build_configs(system_mem_size_gb: int) -> Dict[str, str]:
+    """
+    Returns a dictionary of PostgreSQL configurations as guided by pgtune.
+
+    Reference: https://github.com/le0pard/pgtune/blob/master/src/features/configuration/configurationSlice.js
+    """
+    mem_mb = system_mem_size_gb * 1024
+    return {
+        "shared_buffers": f"{(mem_mb * 0.25):.0f}MB",
+        "effective_cache_size": f"{(mem_mb * 0.75):.0f}MB",
+        "maintenance_work_mem": f"{(mem_mb / 16):.0f}MB",
+        "random_page_cost": "1.1",
+        "checkpoint_completion_target": "0.9",
+        "effective_io_concurrency": "200",
+        "min_wal_size": "4GB",
+        "max_wal_size": "16GB",
+    }
+
+
+def pg_apply_configs(target: PgTarget, configs: Dict[str, str], log: Optional[LogConfig] = None) -> subprocess.CompletedProcess:
+    """
+    Applies the configurations to the PostgreSQL database.
+    """
+    return pg_queries(target, [
+        f"ALTER SYSTEM SET {k} = '{v}';" for k, v in configs.items()
+    ], log)
+
+
+def pg_analyze_waits(target: PgTarget, log: Optional[LogConfig] = None) -> subprocess.CompletedProcess:
+    """
+    Analyzes the waits for the PostgreSQL database.
+    """
+    return pg_file(target, Path(__file__).parent / "pg_analyze_waits.sql", log)
---
+++ pylib/target_run.py
@@ -0,0 +1,125 @@
+import subprocess
+import sys
+import tempfile
+import time
+import os
+from dataclasses import dataclass
+from typing import List, Optional
+
+@dataclass
+class LogConfig:
+    log_key: str
+    action: str
+    log_root_dir: str = "logs"
+
+    def log_file_path(self) -> str:
+        return f"{self.log_root_dir}/{self.log_key}/{self.action}.log"
+    
+    def log_folder_path(self) -> str:
+        return f"{self.log_root_dir}/{self.log_key}"
+
+
+def shell_command(command: str, log: Optional[LogConfig] = None) -> subprocess.CompletedProcess:
+    """
+    Executes a single command.
+    """
+    if log:
+        os.makedirs(log.log_folder_path(), exist_ok=True)
+        result = subprocess.run(command, shell=True, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        with open(log.log_file_path(), "a") as f:
+            f.write(result.stdout)
+        return result
+    else:
+        return subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
+
+
+@dataclass
+class SshTarget:
+    user: str
+    password: Optional[str] = None
+    hostname: str = "localhost"
+    port: int = 22
+    shell: str = "bash"
+
+
+def ssh_command(target: SshTarget, command: str, log: Optional[LogConfig] = None) -> subprocess.CompletedProcess:
+    """
+    Executes a single command on a remote server.
+    """
+    return ssh_commands(target, [command], log)
+
+
+def ssh_commands(target: SshTarget, commands: List[str], log: Optional[LogConfig] = None) -> subprocess.CompletedProcess:
+    """
+    Executes a list of commands on a remote server.
+    """
+    if target.password is None:
+        ssh_cmd = f"ssh {target.user}@{target.hostname} -p {target.port}"
+    else:
+        ssh_cmd = f"sshpass -p {target.password} ssh {target.user}@{target.hostname} -p {target.port}"
+
+    file = tempfile.NamedTemporaryFile(delete=False)
+    file.write("\n".join(commands).encode("utf-8"))
+    file.flush()
+    file.close()
+    
+    result = shell_command(f"cat {file.name} | {ssh_cmd} '{target.shell} -s'", log)
+    os.unlink(file.name)
+    return result
+
+
+@dataclass
+class PgTarget:
+    username: str
+    password: Optional[str] = None
+    hostname: str = "localhost"
+    port: int = 5432
+    database: str = "postgres"
+
+
+def pg_query(target: PgTarget, query: str, log: Optional[LogConfig] = None) -> subprocess.CompletedProcess:
+    """
+    Executes a single query in a single psql session.
+    """
+    pg_queries(target, [query], log)
+
+
+def pg_queries(target: PgTarget, queries: List[str], log: Optional[LogConfig] = None) -> subprocess.CompletedProcess:
+    """
+    Executes a list of queries in a single psql session.
+    """
+    file = tempfile.NamedTemporaryFile(delete=False)
+    file.write("\n".join(queries).encode("utf-8"))
+    file.flush()
+    file.close()
+
+    result = pg_file(target, file.name, log)
+    os.unlink(file.name)
+    return result
+
+
+def pg_file(target: PgTarget, filename: str, log: Optional[LogConfig] = None) -> subprocess.CompletedProcess:
+    """
+    Executes a file in a single psql session.
+    """
+    if target.password is not None:
+        prev_password = os.getenv("PGPASSWORD")
+        os.environ["PGPASSWORD"] = target.password
+
+    pg_cmd = f"psql -U {target.username} -h {target.hostname} -p {target.port} -d {target.database} -f {filename}"
+    result = shell_command(pg_cmd, log)
+
+    if target.password is not None and prev_password is not None:
+        os.environ["PGPASSWORD"] = prev_password
+
+    return result
+
+
+def pg_query_by_ssh(ssh_target: SshTarget, db_name: str, query: str, log: Optional[LogConfig] = None) -> subprocess.CompletedProcess:
+    """
+    Executes a single query in a single psql session on a remote server.
+
+    Assumes that the pg user is 'postgres' and the SshTarget user can sudo into postgres user without a password.
+    """
+    return ssh_command(ssh_target, f"sudo -u postgres psql -d {db_name} -c '{query}'", log)
+
